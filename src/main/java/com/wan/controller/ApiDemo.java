98467314d839b8dc2fcfca85f18702b512fa9c82
Jensen <wangongxun@tehang.com>
2019-04-04 00:58:14 +0800

    删除无用的demo类

--- src/main/java/com/wan/controller/ApiDemo.java
+++ src/main/java/com/wan/controller/ApiDemo.java
@@ -1,12 +1,50 @@
 package com.wan.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.fdd.api.client.dto.BaseDTO;
+import com.wan.utils.CMD;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static io.restassured.RestAssured.given;
 
 @RestController
 @Slf4j
 public class ApiDemo {
 
+    @PostMapping("/account/register")
+    public String test2(String type,String account,String url){
+
+        String timeStamp = CMD.getFormatTimeStamp();
+
+
+        Map<String, String> businessParams = new TreeMap<>();
+        Map<String,String> params=new HashMap();
+
+        businessParams.put("type",type);
+        businessParams.put("account",account);
+
+        BaseDTO sign = CMD.getSign(timeStamp,businessParams);
+
+        params.put("appId","100000");
+        params.put("signType","SHA256");
+        params.put("timestamp",timeStamp);
+        params.put("sign",sign.getSign());
+        params.put("bizContent",sign.getBizContent());
+
+
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
+        Response response = given().contentType(ContentType.JSON).body(jsonObject).post(url);
+        return response.asString();
+    }
 
 
 }
