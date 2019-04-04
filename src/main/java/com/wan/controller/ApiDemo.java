76460b556873dcbdb6e2c67bdc5cde7f4673113b
wan <1014969243@qq.com>
2019-04-04 12:52:28 +0800

    优化ApiDemo 更新CMD

--- src/main/java/com/wan/controller/ApiDemo.java
+++ src/main/java/com/wan/controller/ApiDemo.java
@@ -2,17 +2,15 @@ package com.wan.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.fdd.api.client.dto.BaseDTO;
 import com.wan.utils.CMD;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 import static io.restassured.RestAssured.given;
 
@@ -21,28 +19,13 @@ import static io.restassured.RestAssured.given;
 public class ApiDemo {
 
     @PostMapping("/account/register")
-    public String test2(String type,String account,String url){
-
-        String timeStamp = CMD.getFormatTimeStamp();
+    public String register(@RequestParam Map<String, String> paramMap) {
 
 
-        Map<String, String> businessParams = new TreeMap<>();
-        Map<String,String> params=new HashMap();
+        String url = paramMap.get("url");
 
-        businessParams.put("type",type);
-        businessParams.put("account",account);
-
-        BaseDTO sign = CMD.getSign(timeStamp,businessParams);
-
-        params.put("appId","100000");
-        params.put("signType","SHA256");
-        params.put("timestamp",timeStamp);
-        params.put("sign",sign.getSign());
-        params.put("bizContent",sign.getBizContent());
-
-
-        JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(params));
-        Response response = given().contentType(ContentType.JSON).body(jsonObject).post(url);
+        Response response = given().contentType(ContentType.JSON).body(CMD.getParams(paramMap)).post(url);
+        System.err.println("实际返回结果::" + response.asString());
         return response.asString();
     }
 
